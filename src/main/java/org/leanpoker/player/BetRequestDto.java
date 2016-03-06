897ee4bdd32794845b4c2a67026e912a0a77b190
rgrebski <radoslaw.grebski@gtech.com>
2016-03-06 11:25:01 +0100

    Added BetRequestDto.java

--- src/main/java/org/leanpoker/player/BetRequestDto.java
+++ src/main/java/org/leanpoker/player/BetRequestDto.java
@@ -4,6 +4,13 @@ public class BetRequestDto {
 
     private String tournament_id;
     private String game_id;
+    private Integer round;
+    private Integer bet_index;
+    private Integer small_blind;
+    private Integer orbits;
+    private Integer dealer;
+    private Integer current_buy_in;
+    private Integer pot;
 
     public String getTournament_id() {
         return tournament_id;
@@ -19,5 +26,61 @@ public class BetRequestDto {
 
     public void setGame_id(String game_id) {
         this.game_id = game_id;
+    }
+
+    public Integer getRound() {
+        return round;
+    }
+
+    public void setRound(Integer round) {
+        this.round = round;
+    }
+
+    public Integer getBet_index() {
+        return bet_index;
+    }
+
+    public void setBet_index(Integer bet_index) {
+        this.bet_index = bet_index;
+    }
+
+    public Integer getSmall_blind() {
+        return small_blind;
+    }
+
+    public void setSmall_blind(Integer small_blind) {
+        this.small_blind = small_blind;
+    }
+
+    public Integer getOrbits() {
+        return orbits;
+    }
+
+    public void setOrbits(Integer orbits) {
+        this.orbits = orbits;
+    }
+
+    public Integer getDealer() {
+        return dealer;
+    }
+
+    public void setDealer(Integer dealer) {
+        this.dealer = dealer;
+    }
+
+    public Integer getCurrent_buy_in() {
+        return current_buy_in;
+    }
+
+    public void setCurrent_buy_in(Integer current_buy_in) {
+        this.current_buy_in = current_buy_in;
+    }
+
+    public Integer getPot() {
+        return pot;
+    }
+
+    public void setPot(Integer pot) {
+        this.pot = pot;
     }
 }
