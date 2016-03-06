f69dbd86890082ac8d61e76016a638c7fd28f822
rgrebski <radoslaw.grebski@gtech.com>
2016-03-06 11:54:42 +0100

    Updated player

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -7,8 +7,29 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+        BetRequestDto betRequestDto = new BetRequestDto();
+
+
+        if (firstDraw(betRequestDto)) {
+//            return handleFirstDraw();
+        }
+
+
         return 1000;
     }
+
+    private static boolean firstDraw(BetRequestDto betRequestDto) {
+        return betRequestDto.getRound() == 0;
+    }
+
+//    private static boolean handleFirstDraw() {
+//        if (pairOnHand) {
+//            return true;
+//        }
+//
+//        if (bothCardsSum > 20)//10 10)
+//            betMax1000 return false;
+//    }
 
     public static void showdown(JsonElement game) {
     }
