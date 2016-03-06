ac61f55464eb72d0b78a46b33e0b4a4076bcd487
Rafal Gorzkowski <rafal.gorzkowski@gtech.com>
2016-03-06 11:22:07 +0100

    Suit enum

---
+++ src/main/java/org/leanpoker/player/Suit.java
@@ -0,0 +1,18 @@
+package org.leanpoker.player;/*
+ * This document set is the property of GTECH Corporation, West Greenwich,
+ * Rhode Island, and contains confidential and trade secret information.
+ * It cannot be transferred from the custody or control of GTECH except as
+ * authorized in writing by an officer of GTECH. Neither this item nor
+ * the information it contains can be used, transferred, reproduced, published,
+ * or disclosed, in whole or in part, directly or indirectly, except as
+ * expressly authorized by an officer of GTECH, pursuant to written agreement.
+ *
+ * Copyright 2016 GTECH Corporation. All Rights Reserved.
+ */
+
+public enum Suit {
+    clubs,
+    spades,
+    hearts,
+    diamonds
+}
